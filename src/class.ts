0be1a6a0d9f38a02c51b5095d939e0318a4f08d9
Mysteryeditor <sri027.cvb@gmail.com>
2023-03-15 10:16:21 +0530

    ts task1 commit

---
+++ src/class.ts
@@ -0,0 +1,23 @@
+"use strict";
+class Employee {
+     public empname: string;
+     public empid: number | string;
+     public empdesignation: string;
+     public empsalary: number;
+     empproject?: string;
+     constructor(empname: string, empid: number, empdesignation: string, empsalary: number, empproject: string) {
+         this.empname = empname;
+         this.empid = empid;
+         this.empdesignation = empdesignation;
+         this.empsalary = empsalary;
+         this.empproject = empproject;
+     }
+     public displayEmpDetails() {
+         let result = ["Employee Name:", this.empname, "Employee ID:", this.empid, "Employee Designation:", this.empdesignation, "Employee Salary:", this.empsalary, "Employee Project:", this.empproject];
+         let str = result.toString();
+         console.log(str)
+     }
+ }
+     let empObj = new Employee("Srikanth", 690179, "Ui/Ux Designer", 60000, "AI");
+    
+   empObj.displayEmpDetails();
